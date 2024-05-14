c23bcb816f0189c52325bc80be1058d340e81ff3
firyalshafa <shafafiryal782@gmail.com>
2024-05-14 16:42:05 +0700

    membuat chain function

---
+++ chain function/chain function.cpp
@@ -0,0 +1,33 @@
+// chain function.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+class buku
+{
+	string judul;
+public:
+	buku setJudul(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+} bukunya;
+
+
+int main()
+{
+	// bukunya.setJudul("Matematia");
+	//cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+	return 0;
+
+}
+
+
